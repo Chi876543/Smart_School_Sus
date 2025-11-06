886ea64914c49368143f952a1893e381fce5daa9
Hello <chi876543@gmail.com>
2025-11-06 14:43:56 +0700

    update tracking module

--- ssb-backend/src/modules/tracking.module.ts
+++ ssb-backend/src/modules/tracking.module.ts
@@ -1,31 +1,17 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
+import { DatabaseModule } from './database.module';
 import { TrackingController } from '../controllers/tracking.controller';
 import { TrackingService } from '../services/tracking.service';
-import { TrackingRepository } from '../repository/tracking.repository';
-import { Bus, BusSchema } from '../schema/bus.schema';
-import { Driver, DriverSchema } from '../schema/driver.schema';
-import { Stop, StopSchema } from '../schema/stop.schema';
-import { Route, RouteSchema } from '../schema/route.schema';
-import { Schedule, ScheduleSchema } from '../schema/schedule.schema';
-import { Trip, TripSchema } from '../schema/trip.schema';
-import { Student, StudentSchema } from '../schema/student.schema';
+import { BusRepository } from '../repositories/bus.repository';
+import { DriverRepository } from '../repositories/driver.repository';
+import { RouteRepository } from '../repositories/route.repository';
+import { ScheduleRepository } from '../repositories/schedule.repository';
+import { TripRepository } from '../repositories/trip.repository';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: Bus.name, schema: BusSchema },
-      { name: Driver.name, schema: DriverSchema },
-      { name: Stop.name, schema: StopSchema },
-      { name: Route.name, schema: RouteSchema },
-      { name: Schedule.name, schema: ScheduleSchema },
-      { name: Trip.name, schema: TripSchema },
-      { name: Student.name, schema: StudentSchema },
-    ]),
-  ],
+  imports: [DatabaseModule],
   controllers: [TrackingController],
-  providers: [TrackingService, TrackingRepository],
+  providers: [TrackingService, ScheduleRepository, BusRepository, DriverRepository, RouteRepository, TripRepository],
   exports: [TrackingService], 
 })
 export class TrackingModule {}
--- ssb-backend/src/services/tracking.service.ts
+++ ssb-backend/src/services/tracking.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { TrackingRepository } from '../repository/tracking.repository';
 import { TrackingLocationDto } from '../dtos/tracking-location.dto';
 import { TrackingDetailDto } from 'src/dtos/tracking-detail.dto';
+import { ScheduleRepository } from '../repositories/schedule.repository';
+import { BusRepository } from '../repositories/bus.repository';
+import { DriverRepository } from '../repositories/driver.repository';
+import { RouteRepository } from '../repositories/route.repository';
+import { TripRepository } from '../repositories/trip.repository';
 import axios from 'axios';
 
 @Injectable()
 export class TrackingService {
-  constructor(private readonly trackingRepo: TrackingRepository) {}
+  constructor(
+    private readonly scheduleRepo: ScheduleRepository,
+    private readonly busRepo: BusRepository,
+    private readonly driverRepo: DriverRepository,
+    private readonly routeRepo: RouteRepository,
+    private readonly tripRepo: TripRepository
+  ) {}
 
   // Lấy danh sách xe đang hoạt động (Schedule active/planned)
   async getAllBusLocations() {
-    const schedules = await this.trackingRepo.findActiveSchedules();
+    const schedules = await this.scheduleRepo.findActiveSchedules();
     const results: any[] = [];
 
     for (const s of schedules) {
       if (!s.busId) continue;
-      const bus = await this.trackingRepo.findBusById(s.busId.toString());
+      const bus = await this.busRepo.findBusById(s.busId.toString());
       if (!bus) continue;
 
       results.push({
@@ -33,17 +43,17 @@ export class TrackingService {
 
   // Xem chi tiết 1 xe theo schedule
   async getBusDetail(scheduleId: string): Promise<TrackingDetailDto> {
-    const schedule = await this.trackingRepo.findScheduleById(scheduleId);
+    const schedule = await this.scheduleRepo.findScheduleById(scheduleId);
     if (!schedule) throw new Error('Không tìm thấy Schedule');
 
     const [bus, driver, route] = await Promise.all([
       schedule.busId
-        ? this.trackingRepo.findBusById(schedule.busId.toString())
+        ? this.busRepo.findBusById(schedule.busId.toString())
         : Promise.resolve(null),
       schedule.driverId
-        ? this.trackingRepo.findDriverById(schedule.driverId.toString())
+        ? this.driverRepo.findDriverById(schedule.driverId.toString())
         : Promise.resolve(null),
-      this.trackingRepo.findRouteById(schedule.routeId.toString()),
+      this.routeRepo.findRouteById(schedule.routeId.toString()),
     ]);
 
     if (!bus || !driver || !route) {
@@ -51,7 +61,7 @@ export class TrackingService {
     }
 
     // Lấy danh sách các điểm dừng
-    const stops = await this.trackingRepo.findStopsByRouteId(schedule.routeId.toString());
+    const stops = await this.routeRepo.findStopsByRouteId(schedule.routeId.toString());
     if (stops.length === 0) throw new Error('Tuyến không có điểm dừng');
 
     // Hàm tính khoảng cách
@@ -100,7 +110,7 @@ export class TrackingService {
     }
 
     // Lấy danh sách học sinh
-    const students = await this.trackingRepo.findTripStudents(scheduleId);
+    const students = await this.tripRepo.findTripStudents(scheduleId);
     return {
       scheduleId: scheduleId,
       plateNumber: bus.plateNumber,
@@ -120,7 +130,7 @@ export class TrackingService {
   // Cập nhật vị trí xe buýt
   async updateBusLocation(dto: TrackingLocationDto) {
     const { busId, lat, lng, speed } = dto;
-    const bus = await this.trackingRepo.updateBusLocation(busId, lat, lng, speed);
+    const bus = await this.busRepo.updateBusLocation(busId, lat, lng, speed);
     if (!bus) throw new Error('Không tìm thấy xe buýt');
 
     return {
