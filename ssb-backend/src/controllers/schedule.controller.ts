6a393bdace9cddc669d39fd4fbb3b48129614e45
Hello <chi876543@gmail.com>
2025-11-20 00:53:50 +0700

    add comment Schedulecontroller

--- ssb-backend/src/controllers/schedule.controller.ts
+++ ssb-backend/src/controllers/schedule.controller.ts
@@ -16,36 +16,43 @@ import { ScheduleService } from 'src/services/schedule.service';
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
+  // Lấy danh sách Route hộ trợ cho việc tạo và cập nhập  lịch trình
   @Get('routes')
   async getAllRoutes() {
     return this.scheduleService.findAllRoute();
   }
 
+  // Lấy danh sách timeTables hộ trợ cho việc tạo và cập nhập lịch trình
   @Get('timetables')
   async getAllTimetables() {
     return this.scheduleService.findAllTimetable();
   }
 
+  // Tạo schedule mới (chưa có phân công ), trạng thái unassign
   @Post('create')
   create(@Body() createDto: CreateScheduleDTO) {
     return this.scheduleService.create(createDto);
   }
 
+  // Lấy tất cả các Schedule để hiện thị danh sách
   @Get()
   findAll() {
     return this.scheduleService.findAll();
   }
 
+  // Lấy thông tin chi tiết của 1 schedule (truyền vào ScheduleId)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.scheduleService.findOne(id);
   }
 
+  // Cập nhập Schedule
   @Put(':id')
   update(@Param('id') id: string, @Body() updateDto: UpdateScheduleDTO) {
     return this.scheduleService.update(id, updateDto);
   }
 
+  // Xóa lịch trình (trạng thái cancel)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.scheduleService.remove(id);
