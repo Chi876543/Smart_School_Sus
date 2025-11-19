eb7ab201bc3c8a3c29d74cd21ef40b939b97c046
Hello <chi876543@gmail.com>
2025-11-19 21:30:06 +0700

    fix scheduleModule

--- ssb-backend/src/modules/schedule.module.ts
+++ ssb-backend/src/modules/schedule.module.ts
@@ -2,11 +2,18 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database.module";
 import { ScheduleController } from "src/controllers/schedule.controller";
 import { ScheduleService } from "src/services/schedule.service";
+import { ScheduleRepository } from "src/repositories/schedule.repository";
+import { StudentRepository } from "src/repositories/student.repository";
+import { RouteRepository } from "src/repositories/route.repository";
+import { TimetableRepository } from "src/repositories/timetable.repository";
+import { TripRepository } from "src/repositories/trip.repository";
+import { DriverRepository } from "src/repositories/driver.repository";
+import { BusRepository } from "src/repositories/bus.repository";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [ScheduleController],
-    providers: [ScheduleService],
+    providers: [ScheduleService, ScheduleRepository, StudentRepository, DriverRepository, BusRepository, RouteRepository, TimetableRepository, TripRepository],
 })
 
 export class ScheduleModule {}
